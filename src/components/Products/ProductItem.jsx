052bdd20a2e662d1052d3ac9dfbbfd361ce771f1
Melih DİNÇER <m_dincer41@hotmail.com>
2025-02-08 22:28:27 +0300

    Image boyutu ayarlandı.

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -21,7 +21,7 @@ const ProductItem = ({product, products, setProducts}) => {
     return(
       <div className="col-4">
         <div className="card">
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img id='image' src={imageUrl} className="card-img-top" alt="..."/>
   <ProductInfo>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
